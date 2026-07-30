e77b274ed2699ec3cbf309b04dcea996a4665d35
Kauan Richard Montalvâo Dos Santos <kauanrachando@gmail.com>
2026-07-30 20:30:09 -0300

    Adicionar categorias aos serviços

--- servicos/models.py
+++ servicos/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    ativo = models.BooleanField(default=True)
+
+
 class Servico(models.Model):
     nome = models.CharField(max_length=100)
     valor = models.DecimalField(max_digits=8, decimal_places=2)
     duracao = models.IntegerField()
+    categoria = models.ForeignKey(
+        Categoria,
+        on_delete=models.CASCADE,
+        null=True
+    )
     ativo = models.BooleanField(default=True)
