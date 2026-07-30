c2e6c2045adddefb44fd9e99296b88ec2d2d5f7b
Kauan Richard Montalvâo Dos Santos <kauanrachando@gmail.com>
2026-07-30 20:38:25 -0300

    Adicionar relação profissional e serviços

--- profissionais/models.py
+++ profissionais/models.py
@@ -1,7 +1,8 @@
 from django.db import models
-
+from servicos.models import Servico
 
 class Profissional(models.Model):
     nome = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20)
     ativo = models.BooleanField(default=True)
+    servicos = models.ManyToManyField(Servico)
